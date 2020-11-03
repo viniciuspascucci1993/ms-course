deac8faaa9627ef6e262a0dedc192b0241cfdd4f
Vinicius Pascucci <vinicius.pascucci1@gmail.com>
2020-11-03 18:23:37 -0300

    Zull Timeout

--- hr-worker/src/main/java/com/vinicius/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/vinicius/hrworker/resources/WorkerResource.java
@@ -40,12 +40,11 @@ public class WorkerResource {
 	@RequestMapping( value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<Worker> findById( @PathVariable Long id ) {
 		
-		/*try {
+		try {
 			Thread.sleep(3000L);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
-		}*/
-		
+		}
 		
 		logger.info("PORT: " + env.getProperty("local.server.port"));
 		
